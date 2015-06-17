499100d8c53286440bd4b191a7d2a8f98f3a44f5
sagargaur <sagar.gaur@students.iiit.ac.in>
2015-06-17 10:29:45 +0530

    Engine file, with simple plan

---
+++ engine/engine.py
@@ -0,0 +1,30 @@
+''' Engine to create/update the page 
+What should the engine have?
+	A parser to read decode the files 
+	A module to generate the HTML
+	A controller to handle I/O
+
+OOPS?
+Not required; program is pretty simple and already modular. 
+'''
+
+
+import sys, os, re, linecache
+
+def parser():
+	''' Should allow upgrades; goes in a different file '''
+	pass
+
+def io():
+	''' There should be a method to handle, esp. while writing, file heirarchies '''
+	pass
+
+def generateHTML():
+	''' Should be a seperate file'''
+	pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
